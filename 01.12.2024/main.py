70bd79f1754f3ff7707fac0dc8de63d159b89cc3
Max Schmidt <max.schmidt@blunck-org.de>
2024-12-02 11:06:37 +0100

    made a function for solution 1

--- 01.12.2024/main.py
+++ 01.12.2024/main.py
@@ -1,31 +1,36 @@
 file = open("input.prod", "r")
 input = file.readlines()
 file.close()
-lines = []
-for i in input:
-    lines.append(i[:-1]) # removes the "\n" at the end of each line
-list_left = []
-list_right = []
 
-for i in lines:
-    list_left.append(int(i[0:5]))
-    list_right.append(int(i[6:13]))
+def main_01(data):
+    lines = []
+    for i in data:
+        lines.append(i[:-1]) # removes the "\n" at the end of each line
+    list_left = []
+    list_right = []
 
-# sort both lists from smallest to largest
-list_left.sort()
-list_right.sort()
+    for i in lines:
+        list_left.append(int(i[0:5]))
+        list_right.append(int(i[6:13]))
 
-differences = []
+    # sort both lists from smallest to largest
+    list_left.sort()
+    list_right.sort()
 
-for i in range(len(list_left)):
-    if list_left[i] > list_right[i]:
-        differences.append(list_left[i] - list_right[i])
-    else:
-        differences.append(list_right[i] - list_left[i])
+    differences = []
 
-# add all differences together
-sum = 0
-for i in differences:
-    sum += i
+    for i in range(len(list_left)):
+        if list_left[i] > list_right[i]:
+            differences.append(list_left[i] - list_right[i])
+        else:
+            differences.append(list_right[i] - list_left[i])
 
-print(sum)
+    # add all differences together
+    sum = 0
+    for i in differences:
+        sum += i
+
+    print(sum)
+
+if __name__ == "__main__":
+    main_01(input)
