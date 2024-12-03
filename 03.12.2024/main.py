f1cd30a3311ef3c9c2313089b527d1860e394cd2
Max Schmidt <max.schmidt@blunck-org.de>
2024-12-03 12:21:13 +0100

    changed readme.md and prepped D3

---
+++ 03.12.2024/main.py
@@ -0,0 +1,14 @@
+def get_file():
+    file = open("input.test", "r")
+    data = file.readlines()
+    file.close()
+
+    # Clean the data
+    input = []
+    for i in data:
+        input.append(i[:-1]) # removes the "\n" at the end of each line
+
+    return input
+
+print(get_file())
+
--- readme.md
+++ readme.md
@@ -5,7 +5,7 @@ Since im not an professional programmer please forgive my bad code.
 
 # Solved
 Day 01: ⭐ ⭐\
-Day 02:\
+Day 02: ⭐ ⭐\
 Day 03:\
 Day 04:\
 Day 05:\
