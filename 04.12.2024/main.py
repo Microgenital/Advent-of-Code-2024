356ee23b574be0e6fe2db30b8033c1c118b350c2
Max Schmidt <max.schmidt@blunck-org.de>
2024-12-04 09:36:16 +0100

    prepped D4

--- 04.12.2024/main.py
+++ 04.12.2024/main.py
@@ -1,4 +1,12 @@
 def get_file():
-    file = open("input.prod", "r")
+    file = open("input.test", "r")
     data = file.readlines()
     file.close()
+
+    input = []
+    for i in data:
+        input.append(i[:-1])  # removes the "\n" at the end of each line
+
+    return input
+
+print(get_file())
