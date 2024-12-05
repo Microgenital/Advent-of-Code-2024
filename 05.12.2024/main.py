e492885ba139548206a4be6ad2220fff75fe8415
Max Schmidt <darkblade4k@gmail.com>
2024-12-05 21:51:00 +0100

    fixxed the bug with the wrong inputs, but there is still something wrong

--- 05.12.2024/main.py
+++ 05.12.2024/main.py
@@ -3,17 +3,19 @@ def get_file():
     data = file.readlines()
     file.close()
 
-    input = []
+    input_data = []
     for i in data:
-        input.append(i[:-1])  # removes the "\n" at the end of each line
+        input_data.append(i[:-1])  # removes the "\n" at the end of each line
 
-    return input
+    return input_data
 
-def format(list):
+
+def format_list(input_list):
     # Somewhere in the input list is a blank line, its splits the lists there an returns 2 separate lists
-    for i in range(len(list)):
-        if list[i] == "":
-            return list[:i], list[i+1:]
+    for i in range(len(input_list)):
+        if input_list[i] == "":
+            return input_list[:i], input_list[i + 1:]
+
 
 def is_update_valid(update_list, rules):
     # Check if an update is valid according to the rules
@@ -21,29 +23,34 @@ def is_update_valid(update_list, rules):
         x, y = rule.split("|")
         if x in update_list and y in update_list:
             # Get the indices of the pages in the update list
-            idx_x = update_list.index(x)
-            idx_y = update_list.index(y)
-            if idx_x >= idx_y:
+            idx = update_list.index(x)
+            idy = update_list.index(y)
+            if idx > idy:
                 return False  # Rule violated
     return True
 
+
 def get_middle_number(input_list):
+    input_list = input_list[0].split(",")
+    print(input_list[len(input_list) // 2])
     return input_list[len(input_list) // 2]
+
 
 def main():
     input_data = get_file()
-    update_list, rules = format(input_data)
-    print(update_list)
-    print(rules)
+    rules, update_list = format_list(input_data)
 
     total_middle_sum = 0
     for i in range(1, len(update_list)):
-        if is_update_valid(rules[:i], update_list):
-            middle_number = int(get_middle_number(update_list[:i]))
+        # print(update_list[i])
+        if is_update_valid(update_list[i], rules):
+            middle_number = int(get_middle_number(update_list[i]))
             total_middle_sum += middle_number
     print(total_middle_sum)
 
-    # FIXME: something with "update_list" and "rules" is wrong, need to figure out what is what
+
+# FIXME: i dont know whats wrong, need to look at it tomorrow, should be getting 143, but gets 16
+
 
 if __name__ == "__main__":
- main()
+    main()
